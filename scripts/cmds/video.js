e676a811b74ae8a002177e6c519b977f9fec26f1
kouwachihhh-ops <kouwachihhh@gmail.com>
2026-07-25 17:07:12 +0100

    Update video.js

--- scripts/cmds/video.js
+++ scripts/cmds/video.js
@@ -9,7 +9,7 @@ const baseApiUrl = async () => {
 
 module.exports = {
         config: {
-                name: "video",
+                name: "فيديو",
                 aliases: ["ভিডিও"],
                 version: "1.7",
                 author: "MahMUD",
