dfe7982b45df72e032c107d4fac7b235c2c826cd
fareskont0-stack <fareskont0@gmail.com>
2026-08-01 19:40:11 +0100

    Update groups.js

--- scripts/cmds/groups.js
+++ scripts/cmds/groups.js
@@ -4,7 +4,7 @@ const { writeFileSync } = require("fs-extra");
 
 module.exports = {
 	config: {
-		name: "groups",
+		name: "قروب",
 		aliases: ["مجموعات", "wlt"],
 		version: "1.7",
 		author: "NTKhang",
@@ -98,8 +98,8 @@ module.exports = {
 
 				const getNames = await Promise.all(removed.map(async tid => {
 					const info = await api.getThreadInfo(tid) || {};
-					return { tid, name: info.threadName || "غير معروف"}}}
-				);
+					return { tid, name: info.threadName || "غير معروف" };
+				}));
 
 				writeFileSync(global.client.dirConfig, JSON.stringify(config, null, 2));
 
