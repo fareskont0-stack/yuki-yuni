40da4f98f4a9300bfccd0e7ec8b71bcdccdee597
fareskont0-stack <fareskont0@gmail.com>
2026-08-06 20:20:21 +0100

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -3,25 +3,32 @@ module.exports = {
     name: "owner",
     version: "1.0",
     author: "Fares",
+    countDown: 5,
+    role: 0,
+    description: "Owner only",
     category: "system",
-    guide: "{pn}owner",
-    shortDescription: "معلومات المطور"
+    guide: "{pn}owner"
   },
 
-  onStart: async function ({ message, event }) {
+  onStart: async function ({ api, event }) {
 
     const OWNER_UID = "61592703210940";
 
-    if (event.senderID === OWNER_UID) {
-      return message.reply("👋 مرحباً بك أيها المطور ❤️");
+    if (event.senderID != OWNER_UID) {
+      return api.sendMessage(
+        "🔒 هذا الأمر خاص بالمطور فقط.\n\n" +
+        "👤 المطور: Fares\n" +
+        "📩 تواصل معه:\n" +
+        "https://www.facebook.com/profile.php?id=61592703210940",
+        event.threadID,
+        event.messageID
+      );
     }
 
-    return message.reply(
-`🔒 هذا الأمر خاص بالمطور فقط.
-
-👤 المطور: Fares Khenchli
-📩 للتواصل:
-https://www.facebook.com/profile.php?id=61592703210940
+    return api.sendMessage(
+      "✅ مرحباً أيها المطور.",
+      event.threadID,
+      event.messageID
     );
   }
 };
